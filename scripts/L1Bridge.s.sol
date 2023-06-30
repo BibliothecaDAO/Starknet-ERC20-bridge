d962c31308b1ea26d5f84e0f394e83ecf2389744
Milan Cermak <milan.cermak@gmail.com>
2023-06-30 17:04:18 +0200

    chore: mainnet addrs

--- scripts/L1Bridge.s.sol
+++ scripts/L1Bridge.s.sol
@@ -5,7 +5,7 @@ import {CREATE3Factory} from "create3-factory/CREATE3Factory.sol";
 
 import "../src/l1/bridge.sol";
 
-string constant SALT = "LORDS";
+string constant SALT = "Dx";
 
 contract GetL1BridgeAddress is Script {
     CREATE3Factory internal constant create3 = CREATE3Factory(0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf);
@@ -31,13 +31,16 @@ contract DeployL1Bridge is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         bytes32 salt = keccak256(bytes(SALT));
-        address starknet = address(0xde29d060D45901Fb19ED6C6e959EB22d8626708e); // goerli
-        address l1Token = address(0x7543919933Eef56f754dAF6835fA97F6dfD785D8); // goerli
-        uint256 l2Bridge = 0x59ed05a42af8cc47269faedf8f025465bdfff8742e07689d9e90f99655f83ff;
 
-        // address starknet = 0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4; // mainnet
-        // address l1Token = 0x686f2404e77ab0d9070a46cdfb0b7fecdd2318b0; // mainnet
-        // address l2Bridge = 0xc0de; // mainnet
+        // GOERLI
+        // address starknet = address(0xde29d060D45901Fb19ED6C6e959EB22d8626708e);
+        // address l1Token = address(0x7543919933Eef56f754dAF6835fA97F6dfD785D8);
+        // uint256 l2Bridge = 0x042331a29c53f6084f08964cbd83b94c1a141e6d14009052d55b03793b21d5b3;
+
+        // MAINNET
+        address starknet = address(0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4);
+        address l1Token = address(0x686f2404e77Ab0d9070a46cdfb0B7feCDD2318b0);
+        uint256 l2Bridge = 0x7c76a71952ce3acd1f953fd2a3fda8564408b821ff367041c89f44526076633;
 
         deployed = create3.deploy(salt, abi.encodePacked(type(LordsL1Bridge).creationCode, abi.encode(starknet, l1Token, l2Bridge)));
 
